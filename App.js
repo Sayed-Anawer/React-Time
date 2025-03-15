a07055800c453828372c3a7557163e33a8e927c4
Sayed-Anawer <sayedanawer99@gmail.com>
2025-03-16 02:31:45 +0600

    React createElement added , not using jsx

---
+++ App.js
@@ -0,0 +1,17 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    "Child 1",
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    "Child 2",
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+]);
+
+// console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
