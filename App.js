47401da7b275694e0f146605bcc7d00980e9f560
Sayed-Anawer <sayedanawer99@gmail.com>
2025-03-18 00:37:50 +0600

    added gitignore files

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const parent = React.createElement("div", { id: "parent" }, [
   ]),
 ]);
 
-// console.log(heading);
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
